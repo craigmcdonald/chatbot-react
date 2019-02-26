6d48da42a406a9c82b5f91eab366221202026aa9
Craig McDonald <craig@lclhst.co>
2019-02-26 12:18:41 +0000

    updated image hover

--- lib/steps_components/custom/ImageElement.jsx
+++ lib/steps_components/custom/ImageElement.jsx
@@ -3,7 +3,11 @@ import defaultTheme from '../../theme';
 
 const ImageElement = styled.img`
   width: 40%;
+  margin: 0 4px 0 4px;
   display: inline-block;
+  cursor: pointer;
+
+  &:hover { opacity: .7; }
 `;
 
 ImageElement.defaultProps = {
