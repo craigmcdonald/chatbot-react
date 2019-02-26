cd011abf33b3b1a5d69252f1f2a3434689f1b945
Craig McDonald <craig@lclhst.co>
2019-02-26 15:07:00 +0000

    changes to triggerNextStep

--- lib/ChatBot.jsx
+++ lib/ChatBot.jsx
@@ -250,7 +250,8 @@ class ChatBot extends Component {
     if (isEnd) {
       this.handleEnd();
     } else if (currentStep.imageGroup && data ) {
-      console.log(currentStep);
+      console.log('now here');
+      console.log(currentStep.images);
       const image = currentStep.images.filter(o => o.value == data.value )[0];
       const trigger = this.getTriggeredStep(image.trigger, currentStep.value);
       delete currentStep.images
