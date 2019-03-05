e52b714ef52889c07e075edb164b32d05cccf91d
Craig McDonald <craig@lclhst.co>
2019-03-05 13:40:12 +0000

    incresed image size

--- lib/steps_components/custom/ImageElement.jsx
+++ lib/steps_components/custom/ImageElement.jsx
@@ -2,8 +2,8 @@ import styled from 'styled-components';
 import defaultTheme from '../../theme';
 
 const ImageElement = styled.img`
-  max-width: 120px;
-  max-height: 156px;
+  max-width: 140px;
+  max-height: 168px;
   padding:10px;
   background-color: #D3D3D3;
   margin: 0 4px 4px 4px;
--- lib/steps_components/custom/ImageGroupContainer.jsx
+++ lib/steps_components/custom/ImageGroupContainer.jsx
@@ -5,6 +5,7 @@ const ImageGroupContainer = styled.div`
   border-radius: 5px;
   box-shadow: rgba(0, 0, 0, 0.15) 0px 1px 2px 0px;
   display: flex;
+  flex-direction: column;
   justify-content: center;
   margin: 0 6px 10px 6px;
   padding: 16px;
